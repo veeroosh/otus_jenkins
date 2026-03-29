8a83b67d4527465e75fdeeacd658ee45ff301e77
latysheva <latysheva@protei.ru>
2026-03-29 21:07:34 +0300

    fix

--- jenkins/pipelines/ui_tests.groovy
+++ jenkins/pipelines/ui_tests.groovy
@@ -3,6 +3,9 @@ import groovy.transform.Field
 @Field
 def testsStat = []
 
+@Field
+def WORKSPACE = pwd()
+
 timeout("1200") {
     node("maven") {
         withBuildUser {
@@ -22,7 +25,6 @@ timeout("1200") {
         }
 
         stage('Running UI tests via ansible') {
-            def WORKSPACE = pwd()
             def state = sh(
                     script: "ansible-playbook -i ${WORKSPACE}/jenkins/playbook/hosts ${WORKSPACE}/jenkins/playbook/tests.yaml --tags ui_test", // --extra-vars browser=${yamlConfig['browser']} --extra-vars browser_version=${yamlConfig['browser_version']}
                     returnStatus: true
